26ca69f392785279db920e8b1611e66e686ef60f
antiapsurd <antiapsurd@antiapsurd.org>
2014-09-21 12:58:24 +0200

    3rd commit

---
+++ workshop.py
@@ -0,0 +1,4 @@
+def python_workshop(num):
+    if num == 1: print 'Radimo stringove'
+    elif num == 2: print  'Radimo domaci string1.py, ucimo liste i pogledati dictionary za domaci'
+    elif num == 3: print 'Radimo domaci liste.py, recnike i funkcije'

195b07aba6413c20104cc0e0b0c0abce0131c955
antiapsurd <antiapsurd@antiapsurd.org>
2014-09-30 22:02:55 +0200

    5th commit, dictionary

--- workshop.py
+++ workshop.py
@@ -1,6 +1,15 @@
 def python_workshop(date):
-    if date == '08.09.2014': print ('Radili tool-ing, napravili okvirni plan, upoznali se sa materijom, poceli tipove podataka')
-    elif date == '15.09.2014': print  ('Razjasnili nejasnoce oko domaceg string.py, ucili liste i pogledati dictionary za domaci, imali kratku samorefleksiju')
-    elif date == '22.09.2014': print ('Radili domaci list.py, recnike i funkcije, igrali se sa debagerom')
-    elif date == '29.09.2014': print ('Radili domaci wordcount.py, regularne izraze, poceli rekurzivne funkcije, puskice ;)')	
-    elif date == '06.10.2014': print('')
+    d = {
+    '08.09.2014': 'Radili tool-ing, napravili okvirni plan, upoznali se sa materijom, poceli tipove podataka',
+    '15.09.2014': 'Razjasnili nejasnoce oko domaceg string.py, ucili liste i pogledati dictionary za domaci, imali kratku samorefleksiju',
+    '22.09.2014': 'Radili domaci list.py, recnike i funkcije, igrali se sa debagerom',
+    '29.09.2014': 'Radili domaci wordcount.py, regularne izraze, poceli rekurzivne funkcije, puskice :)',
+    '06.10.2014': '',
+    }
+    if 0:
+      for k,v in d.items(): 
+        print (k,':', v)
+    if 1:
+      for k in d:
+        print(k,':', d[k])
+python_workshop('stagod')

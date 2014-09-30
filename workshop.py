b9bebae2bc56c7a6685189abf6e1db7516dbd3f6
antiapsurd <antiapsurd@antiapsurd.org>
2014-09-30 21:24:48 +0200

    4th commit, ifovi uz printove

--- workshop.py
+++ workshop.py
@@ -1,4 +1,6 @@
-def python_workshop(num):
-    if num == 1: print 'Radimo stringove'
-    elif num == 2: print  'Radimo domaci string1.py, ucimo liste i pogledati dictionary za domaci'
-    elif num == 3: print 'Radimo domaci liste.py, recnike i funkcije'
+def python_workshop(date):
+    if date == '08.09.2014': print ('Radili tool-ing, napravili okvirni plan, upoznali se sa materijom, poceli tipove podataka')
+    elif date == '15.09.2014': print  ('Razjasnili nejasnoce oko domaceg string.py, ucili liste i pogledati dictionary za domaci, imali kratku samorefleksiju')
+    elif date == '22.09.2014': print ('Radili domaci list.py, recnike i funkcije, igrali se sa debagerom')
+    elif date == '29.09.2014': print ('Radili domaci wordcount.py, regularne izraze, poceli rekurzivne funkcije, puskice ;)')	
+    elif date == '06.10.2014': print('')
